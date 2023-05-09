a4156054d40f4de11e74b92170cd10d4c500519a
Yusuf-Kenan <kenan2404@gmail.com>
2023-05-09 17:36:36 +0300

    add product and count total

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const catList = document.querySelector(".cat-list");
-const prodList=document.querySelector(".prod-list")
-const cartButton=document.querySelector("#cart")
-const closeImg=document.querySelector("#close")
-const modalWrapper=document.querySelector(".modal-wrapper")
+const prodList = document.querySelector(".prod-list");
+const cartButton = document.querySelector("#cart");
+const closeImg = document.querySelector("#close");
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modalList = document.querySelector(".modal-list");
+const totalPrice = document.querySelector(".price-total");
 
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    fetchCats()
-    fetchProduct()
+document.addEventListener("DOMContentLoaded", () => {
+  fetchCats();
+  fetchProduct();
 });
 
 function fetchCats() {
@@ -31,12 +32,11 @@ function fetchProduct() {
   fetch("https://api.escuelajs.co/api/v1/products")
     .then((res) => res.json())
     .then((data) => {
-        //proje bitiminde slice silinecek.....
-        data.slice(0,20).forEach((prod)=>{
-           
-            const prodDiv=document.createElement("div")
-            prodDiv.classList.add("prod")
-            prodDiv.innerHTML=`
+      //proje bitiminde slice silinecek.....
+      data.slice(0, 20).forEach((prod) => {
+        const prodDiv = document.createElement("div");
+        prodDiv.classList.add("prod");
+        prodDiv.innerHTML = `
             <img src="${prod.images[1]}" alt="">
                 <p>${prod.title}</p>
                 <p>${prod.category.name}</p>
@@ -45,26 +45,50 @@ function fetchProduct() {
                     <button onclick='addCart({name: "${prod.title}", id: ${prod.id}, price: ${prod.price}, amount:1})'>Add</button>
                 </div>
             `;
-            prodList.appendChild(prodDiv)
-
-        })
+        prodList.appendChild(prodDiv);
+      });
     })
     .catch((err) => console.log(err));
 }
+var cart = [];
+var totalCoast=0;
 
-cartButton.addEventListener("click", toggleCart)
-closeImg.addEventListener("click", toggleCart)
+function addToCart() {
+  cart.forEach((item) => {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+    cartItem.innerHTML = `
+        <h3>${item.name}</h3>
+        <h3>${item.amount}</h3>
+        <h3>€${item.price}</h3>
+        `;
+    modalList.appendChild(cartItem);
 
-function toggleCart(){
-    modalWrapper.classList.toggle("active")
+    totalCoast +=item.price*item.amount
+  });
+  totalPrice.innerText=totalCoast
+}
+
+cartButton.addEventListener("click", () => {
+  toggleCart();
+  addToCart();
+});
+closeImg.addEventListener("click", ()=>{
+    toggleCart();
+    modalList.innerHTML=""
+});
+
+function toggleCart() {
+  modalWrapper.classList.toggle("active");
 }
 
 //Add To Cart
-var cart=[]
-function addCart(param){
-    const addedItem=cart.find(item=>item.id==param.id)
-    if(addedItem){
-        addedItem.amount+=1
-    }else {cart.push(param)}
-console.log(cart)
+
+function addCart(param) {
+  const addedItem = cart.find((item) => item.id == param.id);
+  if (addedItem) {
+    addedItem.amount += 1;
+  } else {
+    cart.push(param);
+  }
 }
